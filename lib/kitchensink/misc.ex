0e8334fecd9f3883768656801495678bd56aa6c0
Paul Iannazzo <somethingitalian@gmail.com>
2016-10-08 13:18:22 -0400

    added nth function. added first..tenth functions

---
+++ lib/kitchensink/misc.ex
@@ -0,0 +1,33 @@
+defmodule KitchenSink.Misc do
+
+  @moduledoc """
+
+  """
+
+  @doc """
+
+  """
+
+  def nth(index) do
+    fn
+      list when is_list(list) ->
+        Enum.at(list, index)
+      tuple when is_tuple(tuple) ->
+        elem(tuple, index)
+      map when is_map(map) ->
+        map |> Stream.take(index + 1) |> Stream.drop(index) |> Enum.to_list |> Map.new
+    end
+  end
+
+  def first(list), do: nth(0).(list)
+  def second(list), do: nth(1).(list)
+  def third(list), do: nth(2).(list)
+  def fourth(list), do: nth(3).(list)
+  def fifth(list), do: nth(4).(list)
+  def sixth(list), do: nth(5).(list)
+  def seventh(list), do: nth(6).(list)
+  def eighth(list), do: nth(7).(list)
+  def nineth(list), do: nth(8).(list)
+  def tenth(list), do: nth(9).(list)
+
+end
---
+++ test/kitchensink_test/misc_test.exs
@@ -0,0 +1,46 @@
+defmodule KitchenSink.MiscTest do
+  @moduledoc false
+
+  use ExUnit.Case
+  import KitchenSink.Misc
+
+  test "curried nth function is put into the module and works as expected" do
+
+
+    tuple = {1, 2 ,3 ,4 ,5}
+    list = [1, 2, 3, 4, 5]
+    map = %{a: 1, b: 2, c: 3, d: 4, e: 5}
+
+    assert nth(0).(tuple) == 1
+    assert nth(0).(list) == 1
+    assert nth(0).(map) == %{a: 1}
+    assert elem(tuple, 0) == 1
+    assert get_in(list, [Access.at(0)]) == 1
+    assert get_in(tuple, [Access.elem(0)]) == 1
+    assert get_in(Map.to_list(map), [Access.at(0)]) |> List.wrap |> Map.new == %{a: 1}
+    assert map |> Enum.take(1) |> List.wrap |> Map.new  == %{a: 1}
+    assert map |> Stream.take(1) |> Enum.to_list |> Map.new  == %{a: 1}
+
+  end
+  test "named nth functions is put into the module and works as expected" do
+    tuple = {1, 2 ,3 ,4 ,5}
+    list = [1, 2, 3, 4, 5]
+    map = %{a: 1, b: 2, c: 3, d: 4, e: 5}
+
+    assert first(tuple) == 1
+    assert first(list) == 1
+    assert first(map) == %{a: 1}
+
+    assert second(tuple) == 2
+    assert second(list) == 2
+    assert second(map) == %{b: 2}
+
+    assert third(tuple) == 3
+    assert third(list) == 3
+    assert third(map) == %{c: 3}
+
+    assert fourth(tuple) == 4
+    assert fourth(list) == 4
+    assert fourth(map) == %{d: 4}
+  end
+end
