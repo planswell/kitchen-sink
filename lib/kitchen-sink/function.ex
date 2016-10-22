4dd23fd4212c4938ba0661c6758af1eb6e415ac1
Paul Iannazzo <somethingitalian@gmail.com>
2016-10-22 19:43:14 -0400

    added identity, compose, pipe, juxt functions.

--- lib/kitchen-sink/function.ex
+++ lib/kitchen-sink/function.ex
@@ -11,4 +11,35 @@ defmodule KitchenSink.Function do
     fn(arg1, arg2) -> fun.(arg2, arg1) end
   end
 
+  def identity(x), do: x
+
+  def compose(functions) when is_list(functions) do
+    functions = Enum.reverse(functions)
+    fn(input) ->
+      Enum.reduce(functions, input, fn (fun, acc) ->
+        fun.(acc)
+      end)
+    end
+  end
+
+  def pipe(functions) when is_list(functions) do
+    fn(input) ->
+      Enum.reduce(functions, input, fn (fun, acc) ->
+        fun.(acc)
+      end)
+    end
+  end
+
+  @doc """
+  from clojure. takes in a list of functions, outputs a function that
+  takes in a list of values, then applies the functions to each
+  value. sorta like a super Enum.map
+  """
+  def juxt(functions) when is_list(functions) do
+    #good for optimiztion
+    super_fun = fn x -> Enum.map(functions, fn fun -> fun.(x) end) end
+    fn(input_list) ->
+      Enum.map(input_list, super_fun)
+    end
+  end
 end
