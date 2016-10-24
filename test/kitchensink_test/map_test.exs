704dd6a0c24535d2252fbe4eba989a53efc9d007
Paul Iannazzo <somethingitalian@gmail.com>
2016-10-24 16:09:07 -0400

    added: Map.make_nested, Map.remap_keys
    make_nested creates a nested Map based of off a list of keys and a
    value.
    remap_keys does a transformation of a map to rename and re-position keys

--- test/kitchensink_test/map_test.exs
+++ test/kitchensink_test/map_test.exs
@@ -2,79 +2,123 @@ defmodule KitchenSink.MapTest do
   @moduledoc false
 
   use ExUnit.Case
-  alias KitchenSink.Map
+  alias KitchenSink.Map, as: KMap
 
   doctest KitchenSink.Map
 
   test "empty list produce empty map" do
-    assert Map.merge([]) == %{}
+    assert KMap.merge([]) == %{}
   end
 
   test "map with 1 element produces that element" do
-    assert Map.merge([%{a: 1}]) == %{a: 1}
+    assert KMap.merge([%{a: 1}]) == %{a: 1}
   end
 
   test "map with 2 element produces the merge of those 2 according to merge/2 rules" do
-    assert Map.merge([%{a: 1}, %{b: 1}]) == %{a: 1, b: 1}
-    assert Map.merge([%{a: 1}, %{a: 2}]) == %{a: 2}
+    assert KMap.merge([%{a: 1}, %{b: 1}]) == %{a: 1, b: 1}
+    assert KMap.merge([%{a: 1}, %{a: 2}]) == %{a: 2}
   end
 
   test "map with 3 element produces the merge of those 3" do
-    assert Map.merge([%{a: 1}, %{b: 1}, %{c: 1}]) == %{a: 1, b: 1, c: 1}
-    assert Map.merge([%{a: 1}, %{b: 1}, %{a: 2}]) == %{a: 2, b: 1}
+    assert KMap.merge([%{a: 1}, %{b: 1}, %{c: 1}]) == %{a: 1, b: 1, c: 1}
+    assert KMap.merge([%{a: 1}, %{b: 1}, %{a: 2}]) == %{a: 2, b: 1}
   end
 
   test "map with 2 element produces the deep_merge of those 2 according to merge/2 rules" do
-    assert Map.deep_merge(%{a: 1}, %{b: 1}) == %{a: 1, b: 1}
-    assert Map.deep_merge(%{a: 1}, %{a: 2}) == %{a: 2}
-    assert Map.deep_merge(%{a: %{b: 1, c: 1}}, %{a: %{b: 2}}) == %{a: %{b: 2, c: 1}}
+    assert KMap.deep_merge(%{a: 1}, %{b: 1}) == %{a: 1, b: 1}
+    assert KMap.deep_merge(%{a: 1}, %{a: 2}) == %{a: 2}
+    assert KMap.deep_merge(%{a: %{b: 1, c: 1}}, %{a: %{b: 2}}) == %{a: %{b: 2, c: 1}}
   end
 
   test "map with many elements produces the deep merge of those 3" do
-    assert Map.deep_merge([%{a: 1}, %{b: 1}, %{c: 1}]) == %{a: 1, b: 1, c: 1}
-    assert Map.deep_merge([%{a: 1}, %{b: 1}, %{a: 2}]) == %{a: 2, b: 1}
-    assert Map.deep_merge(%{a: %{b: 1, c: 1}}, %{a: %{b: 2}}) == %{a: %{b: 2, c: 1}}
-    assert Map.deep_merge([%{a: %{b: 1, c: 1}}, %{a: %{b: 2}}, %{d: 2}]) == %{a: %{b: 2, c: 1}, d: 2}
-    assert Map.deep_merge([%{a: %{b: %{c: %{d: 1}}}}, %{a: %{z: 1}}]) == %{a: %{b: %{c: %{d: 1}}, z: 1}}
+    assert KMap.deep_merge([%{a: 1}, %{b: 1}, %{c: 1}]) == %{a: 1, b: 1, c: 1}
+    assert KMap.deep_merge([%{a: 1}, %{b: 1}, %{a: 2}]) == %{a: 2, b: 1}
+    assert KMap.deep_merge(%{a: %{b: 1, c: 1}}, %{a: %{b: 2}}) == %{a: %{b: 2, c: 1}}
+    assert KMap.deep_merge([%{a: %{b: 1, c: 1}}, %{a: %{b: 2}}, %{d: 2}]) == %{a: %{b: 2, c: 1}, d: 2}
+    assert KMap.deep_merge([%{a: %{b: %{c: %{d: 1}}}}, %{a: %{z: 1}}]) == %{a: %{b: %{c: %{d: 1}}, z: 1}}
   end
 
   test "empty map rename produce empty map" do
-    assert Map.rename_key(%{}, :a, :b) == %{}
-    assert Map.rename_key(%{}, {:a, :b}) == %{}
-    assert Map.rename_key(%{}, %{a: :b}) == %{}
-    assert Map.rename_key(%{}, [a: :b]) == %{}
+    assert KMap.rename_key(%{}, :a, :b) == %{}
+    assert KMap.rename_key(%{}, {:a, :b}) == %{}
+    assert KMap.rename_key(%{}, %{a: :b}) == %{}
+    assert KMap.rename_key(%{}, [a: :b]) == %{}
   end
 
   test "nil rename produce empty map" do
-    assert Map.rename_key(nil, :a, :b) == %{}
-    assert Map.rename_key(nil, {:a, :b}) == %{}
-    assert Map.rename_key(nil, %{a: :b}) == %{}
-    assert Map.rename_key(nil, [a: :b]) == %{}
+    assert KMap.rename_key(nil, :a, :b) == %{}
+    assert KMap.rename_key(nil, {:a, :b}) == %{}
+    assert KMap.rename_key(nil, %{a: :b}) == %{}
+    assert KMap.rename_key(nil, [a: :b]) == %{}
   end
 
   test "rename top-level key in map to same name produces same map" do
-    assert Map.rename_key(%{a: 1}, :a, :a) == %{a: 1}
-    assert Map.rename_key(%{a: 1}, :b, :b) == %{a: 1}
-    assert Map.rename_key(%{a: 1}, {:a, :a}) == %{a: 1}
-    assert Map.rename_key(%{a: 1}, {:b, :b}) == %{a: 1}
-    assert Map.rename_key(%{a: 1}, %{a: :a}) == %{a: 1}
-    assert Map.rename_key(%{a: 1}, %{b: :b}) == %{a: 1}
-    assert Map.rename_key(%{a: 1}, [a: :a]) == %{a: 1}
-    assert Map.rename_key(%{a: 1}, [b: :b]) == %{a: 1}
+    assert KMap.rename_key(%{a: 1}, :a, :a) == %{a: 1}
+    assert KMap.rename_key(%{a: 1}, :b, :b) == %{a: 1}
+    assert KMap.rename_key(%{a: 1}, {:a, :a}) == %{a: 1}
+    assert KMap.rename_key(%{a: 1}, {:b, :b}) == %{a: 1}
+    assert KMap.rename_key(%{a: 1}, %{a: :a}) == %{a: 1}
+    assert KMap.rename_key(%{a: 1}, %{b: :b}) == %{a: 1}
+    assert KMap.rename_key(%{a: 1}, [a: :a]) == %{a: 1}
+    assert KMap.rename_key(%{a: 1}, [b: :b]) == %{a: 1}
   end
 
   test "rename top-level key in map that doesn't exist produces same map" do
-    assert Map.rename_key(%{a: 1}, :b, :c) == %{a: 1}
-    assert Map.rename_key(%{a: 1}, {:b, :c}) == %{a: 1}
-    assert Map.rename_key(%{a: 1}, %{b: :c}) == %{a: 1}
-    assert Map.rename_key(%{a: 1}, [b: :c]) == %{a: 1}
+    assert KMap.rename_key(%{a: 1}, :b, :c) == %{a: 1}
+    assert KMap.rename_key(%{a: 1}, {:b, :c}) == %{a: 1}
+    assert KMap.rename_key(%{a: 1}, %{b: :c}) == %{a: 1}
+    assert KMap.rename_key(%{a: 1}, [b: :c]) == %{a: 1}
   end
 
   test "rename top-level key produce new map with new key" do
-    assert Map.rename_key(%{a: 1}, :a, :b) == %{b: 1}
-    assert Map.rename_key(%{a: 1}, {:a, :b}) == %{b: 1}
-    assert Map.rename_key(%{a: 1}, %{a: :b}) == %{b: 1}
-    assert Map.rename_key(%{a: 1}, [a: :b]) == %{b: 1}
+    assert KMap.rename_key(%{a: 1}, :a, :b) == %{b: 1}
+    assert KMap.rename_key(%{a: 1}, {:a, :b}) == %{b: 1}
+    assert KMap.rename_key(%{a: 1}, %{a: :b}) == %{b: 1}
+    assert KMap.rename_key(%{a: 1}, [a: :b]) == %{b: 1}
+  end
+
+  test "new nested key" do
+    expected = %{parent: %{child: %{conception: "kenbert this is for you"}}}
+    actual = KMap.make_nested([:parent, :child, :conception], "kenbert this is for you")
+
+    assert expected == actual
+  end
+
+  test "rename many keys from strings to atoms" do
+    expected = %{
+      "not remapped" => 0,
+      value: 1,
+      another_value: 2,
+      solo_parent: "divorced",
+      parent: %{
+        possible_bad_merge: "bad merge",
+        child1: "baby",
+        child2: %{conception: "before baby"}},
+    }
+
+    expected2 = Map.delete(expected, "not remapped")
+
+    key_map = %{
+      "Some Value" => :value,
+      "Another Rent" => :another_value,
+      "Parent" => [:solo_parent],
+      "Parent Nested With Child" => [:parent, :child1],
+      "Parent Nested With Nested Child" => [:parent, :child2, :conception],
+      "Merge Test" => [:parent, :possible_bad_merge]
+    }
+
+    input = %{
+      "not remapped" => 0,
+      "Some Value" => 1,
+      "Another Rent" => 2,
+      "Parent" => "divorced",
+      "Parent Nested With Child" => "baby",
+      "Parent Nested With Nested Child" => "before baby",
+      "Merge Test" => "bad merge",
+    }
+
+    assert KMap.remap_keys(input, key_map) == expected
+    assert KMap.remap_keys(input, key_map, prune: true) == expected2
   end
 
   defmodule TestMap do
@@ -85,7 +129,7 @@ defmodule KitchenSink.MapTest do
   test "Clean struct" do
     user1 = %TestMap{name: "User 1"}
     expected = %{name: "User 1"}
-    actual = Map.clean_struct(user1)
+    actual = KMap.clean_struct(user1)
     assert actual == expected
   end
 
@@ -93,7 +137,7 @@ defmodule KitchenSink.MapTest do
     user1 = %TestMap{name: "User 1"}
     user2 = %TestMap{age: 21}
     expected = %TestMap{name: "User 1", age: 21}
-    actual = Map.deep_merge(user1, user2)
+    actual = KMap.deep_merge(user1, user2)
     assert actual == expected
   end
 end
