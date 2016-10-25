748630185f67efc8fb67491fe66125dd2e240f02
Olafur Arason <olafura@olafura.com>
2016-10-25 16:27:14 -0400

    Make credo happy

--- lib/kitchen_sink/boolean.ex
+++ lib/kitchen_sink/boolean.ex
@@ -4,6 +4,18 @@ defmodule KitchenSink.Boolean do
   this module is for Boolean functions
   """
 
+  defp string_bool?("true"), do: true
+  defp string_bool?("false"), do: false
+  defp string_bool?("t"), do: true
+  defp string_bool?("f"), do: false
+  defp string_bool?("on"), do: true
+  defp string_bool?("off"), do: false
+  defp string_bool?("yes"), do: true
+  defp string_bool?("no"), do: false
+  defp string_bool?("y"), do: true
+  defp string_bool?("n"), do: false
+  defp string_bool?(_), do: :error
+
   @doc """
   Parse boolean either from string or integer, if it's not a boolean then it returns :error
   """
@@ -11,19 +23,7 @@ defmodule KitchenSink.Boolean do
     st
     |> String.trim
     |> String.downcase
-    |> case do
-      "true" -> true
-      "false" -> false
-      "t" -> true
-      "f" -> false
-      "on" -> true
-      "off" -> false
-      "yes" -> true
-      "no" -> false
-      "y" -> true
-      "n" -> false
-      _ -> :error
-    end
+    |> string_bool?
   end
   def parse(i) when is_integer(i) do
     case i do
