590ce58c60689088b37cb978262c5b7fc0c31a70
Olafur Arason <olafura@olafura.com>
2016-10-25 18:00:30 -0400

    Implement bool parsing (#10)
    * Implement bool parsing
    
    * Add more truthy and falsy string values
    
    * Add trim
    
    * Make credo happy

---
+++ test/kitchen_sink/boolean_test.exs
@@ -0,0 +1,29 @@
+defmodule KitchenSink.BooleanTest do
+  @moduledoc false
+
+  use ExUnit.Case
+  alias KitchenSink.Boolean
+
+  doctest KitchenSink.Boolean
+
+  test "boolean parse" do
+    assert Boolean.parse("TRUE") === true
+    assert Boolean.parse("FALSE") === false
+    assert Boolean.parse("true") === true
+    assert Boolean.parse("false") === false
+    assert Boolean.parse("t") === true
+    assert Boolean.parse("f") === false
+    assert Boolean.parse("on") === true
+    assert Boolean.parse("off") === false
+    assert Boolean.parse("yes") === true
+    assert Boolean.parse("no") === false
+    assert Boolean.parse("y") === true
+    assert Boolean.parse("n") === false
+    assert Boolean.parse(1) === true
+    assert Boolean.parse(" TRUE ") === true
+    assert Boolean.parse(" FALSE ") === false
+    assert Boolean.parse("falsy") === :error
+    assert Boolean.parse(2) === :error
+  end
+
+end
