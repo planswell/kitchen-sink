8c32b3ea666a274b49614a71b9f337dd9534d858
Aaron Muir Hamilton <aaron@correspondwith.me>
2016-11-29 00:54:45 +0000

    Implement sig_ceil function for ceiling to a significance.

--- lib/kitchen_sink/math.ex
+++ lib/kitchen_sink/math.ex
@@ -13,4 +13,15 @@ defmodule KitchenSink.Math do
   def div(_numerator, 0.0, div_by_zero), do: div_by_zero
   def div(numerator, denominator, _div_by_zero), do: numerator / denominator
 
+  @doc """
+  Always round up to the nearest multiple of significance
+
+  ## Example:
+
+  iex> ceil(31, 5)
+  35
+  """
+  def ceil(number, significance) when is_integer(number) and is_integer(significance) do
+    trunc(Float.ceil(number / significance) * significance)
+  end
 end
