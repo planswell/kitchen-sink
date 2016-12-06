7aa05cb09d6084e23c7e91d3b41da8b43e026b46
Olafur Arason <olafura@olafura.com>
2016-12-06 10:24:09 -0500

    Add Access to Struct (#36)

---
+++ lib/kitchen_sink/struct_access.ex
@@ -0,0 +1,24 @@
+defmodule KitchenSink.StructAccess do
+  @moduledoc """
+
+  This module makes structs use Access to get and change
+  their values.
+
+  To use it for your struct do
+  ```
+  use KitchenSink.StructAccess
+  ```
+
+  """
+
+  defmacro __using__(_opts) do
+    quote do
+      def fetch(map, key) when is_map(map) do
+        Map.fetch(map, key)
+      end
+      def get_and_update(map, key, fun) when is_map(map) do
+        Map.get_and_update(map, key, fun)
+      end
+    end
+  end
+end
