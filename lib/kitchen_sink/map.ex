f4be3638af4a0c90f4f9ca0f4ce09d37cc2f8550
Marius Butuc <contact@mariusbutuc.com>
2016-12-07 14:58:06 -0500

    Explain do_deep_merge variant (#40)
    * Explain do_deep_merge variant
    * Cluster variations of the same function together

--- lib/kitchen_sink/map.ex
+++ lib/kitchen_sink/map.ex
@@ -56,7 +56,9 @@ defmodule KitchenSink.Map do
     right = clean_struct(right)
     do_deep_merge(left, right)
   end
-
+  # When the left argument is a struct, ensure that its own keys are the only
+  # ones considered for the merge.
+  # This guarantees that we don't convert it into a map containing new keys.
   defp do_deep_merge(%{__struct__: _} = left, right) do
     :maps.map(fn key, left_value ->
       case Map.get(right, key) do
@@ -65,7 +67,6 @@ defmodule KitchenSink.Map do
       end
     end, left)
   end
-
   defp do_deep_merge(left, right) do
     Map.merge(left, right, &do_deep_resolve/3)
   end
