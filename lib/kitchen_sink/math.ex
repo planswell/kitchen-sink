70c0898293a7a7dcc5b9546791d0f64e4a572010
Robert Peacock <robertpeacock22@gmail.com>
2017-02-13 14:01:40 -0500

    Add more tests
    Add a guard against accepting 0 for the second parameter of ceil/2.

--- lib/kitchen_sink/math.ex
+++ lib/kitchen_sink/math.ex
@@ -14,17 +14,26 @@ defmodule KitchenSink.Math do
   def div(numerator, denominator, _div_by_zero), do: numerator / denominator
 
   @doc """
-  Always round up to the nearest multiple of significance
+  Always round up to the nearest multiple of significance.
+
+  ## Parameters
+
+    - number: The number you wish to have rounded.
+    - significance: The multiple to which you would like to round.
+                    Must not be 0.
 
   ## Example:
 
-  iex> ceil(31, 5)
+  iex> round_up_to_multiple(31, 5)
   35
 
-  iex> ceil(29.49, 10)
+  iex> round_up_to_multiple(29.49, 10)
   30
   """
-  def ceil(number, significance) when is_integer(significance) do
+  def round_up_to_multiple(_, 0) do
+    raise ArgumentError, message: "Significance multiple cannot be zero."
+  end
+  def round_up_to_multiple(number, significance) when is_integer(significance) do
     trunc(Float.ceil(number / significance) * significance)
   end
 end
--- test/kitchen_sink/math_test.exs
+++ test/kitchen_sink/math_test.exs
@@ -14,14 +14,22 @@ defmodule KitchenSink.MathTest do
     assert 2.0 == div(4.0, 2, 0)
   end
 
-  test "ceil function stays put on zero" do
-    assert ceil(0, 1) == 0
-    assert ceil(0, 3) == 0
-    assert ceil(0, 217) == 0
-  end
+  describe "round_up_to_multiple/2" do
+    test "stays put on zero" do
+      assert round_up_to_multiple(0, 1) == 0
+      assert round_up_to_multiple(0, 3) == 0
+      assert round_up_to_multiple(0, 217) == 0
+    end
 
-  test "negative numbers ceil toward infinity" do
-    assert ceil(-7.5, 5) == -5
-    assert ceil(-11, 5) == -10
+    test "negative numbers round up toward infinity" do
+      assert round_up_to_multiple(-7.5, 5) == -5
+      assert round_up_to_multiple(-11, 5) == -10
+    end
+
+    test "significance multiple must not be zero" do
+      assert_raise ArgumentError, fn ->
+        round_up_to_multiple(5, 0)
+      end
+    end
   end
 end
