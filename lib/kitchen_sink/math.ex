66f75c927eb35f77feab30f0283c3fad278bd42c
Robert Peacock <robertpeacock22@gmail.com>
2017-02-13 11:49:15 -0500

    Allow Kmath.ceil to accept floats (#48)

--- lib/kitchen_sink/math.ex
+++ lib/kitchen_sink/math.ex
@@ -20,8 +20,11 @@ defmodule KitchenSink.Math do
 
   iex> ceil(31, 5)
   35
+
+  iex> ceil(29.49, 10)
+  30
   """
-  def ceil(number, significance) when is_integer(number) and is_integer(significance) do
+  def ceil(number, significance) when is_integer(significance) do
     trunc(Float.ceil(number / significance) * significance)
   end
 end
