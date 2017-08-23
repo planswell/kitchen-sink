aa8a7ba6b54056b05dca06b5f1a20b9c83f8b752
Mike Stok <mike@stok.ca>
2017-08-23 09:50:29 -0400

    Use asdf's .tool-versions to define versions
    This eliminates "unnecessary" work from the circle preparation.

--- circle_pre_build.sh
+++ circle_pre_build.sh
@@ -8,14 +8,7 @@ fi
 # Add plugins for asdf
 asdf plugin-list | grep -q erlang || asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf plugin-list | grep -q elixir || asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
-# Extract vars from elixir_buildpack.config
-. elixir_buildpack.config
-# Write .tool-versions
-echo "erlang $erlang_version" >> .tool-versions
-echo "elixir $elixir_version" >> .tool-versions
-# Install erlang/elixir
-asdf install erlang $erlang_version
-asdf install elixir $elixir_version
+asdf install
 # Get dependencies
 yes | mix deps.get
 yes | mix local.rebar
