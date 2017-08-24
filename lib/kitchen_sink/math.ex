88bba2d60d17c27827080e00e4b5bb7485640f31
Mike Stok <mike@stok.ca>
2017-08-24 15:22:05 -0400

    Add typespecs and doctests to Math

--- lib/kitchen_sink/math.ex
+++ lib/kitchen_sink/math.ex
@@ -4,11 +4,21 @@ defmodule KitchenSink.Math do
   Math functions!
   """
 
-  @doc """
-  handles divide by zero gracefully.
-
-  third argument is what to return when you divide by zero.
+  @typedoc """
+  Some functions allow us to specify *something* to return in a case which would
+  normally raise an error. This type identifies that.
   """
+  @type error_return :: any
+
+  @doc """
+  Numerical division which allows a specified return for "divide by zero."
+
+      iex> div(1, 2, :oops)
+      0.5
+      iex> div(1, 0, :oops)
+      :oops
+  """
+  @spec div(number, number, error_return) :: number | error_return
   def div(_numerator, 0, div_by_zero), do: div_by_zero
   def div(_numerator, 0.0, div_by_zero), do: div_by_zero
   def div(numerator, denominator, _div_by_zero), do: numerator / denominator
@@ -25,14 +35,15 @@ defmodule KitchenSink.Math do
     - significance: The multiple to which you would like to round.
                     Must not be 0.
 
-  ## Example:
+  ## Examples:
 
-  iex> round_down_to_multiple(31, 5)
-  30
+      iex> round_down_to_multiple(31, 5)
+      30
 
-  iex> round_down_to_multiple(29.49, 10)
-  20
+      iex> round_down_to_multiple(29.49, 10)
+      20
   """
+  @spec round_down_to_multiple(number, number) :: number | no_return
   def round_down_to_multiple(_, 0) do
     raise ArgumentError, message: "Significance multiple cannot be zero."
   end
@@ -49,14 +60,15 @@ defmodule KitchenSink.Math do
     - significance: The multiple to which you would like to round.
                     Must not be 0.
 
-  ## Example:
+  ## Examples:
 
-  iex> round_up_to_multiple(31, 5)
-  35
+      iex> round_up_to_multiple(31, 5)
+      35
 
-  iex> round_up_to_multiple(29.49, 10)
-  30
+      iex> round_up_to_multiple(29.49, 10)
+      30
   """
+  @spec round_up_to_multiple(number, number) :: number | no_return
   def round_up_to_multiple(_, 0) do
     raise ArgumentError, message: "Significance multiple cannot be zero."
   end
