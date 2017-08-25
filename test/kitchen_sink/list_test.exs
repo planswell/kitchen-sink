cc269674b04af8f1e555f0c46eca0fa4808e124f
Marius Butuc <marius.butuc@gmail.com>
2017-08-25 14:13:10 -0400

    Add typespecs and doctests to List

--- test/kitchen_sink/list_test.exs
+++ test/kitchen_sink/list_test.exs
@@ -4,24 +4,7 @@ defmodule KitchenSink.ListTest do
   use ExUnit.Case
   alias KitchenSink.List, as: L
 
-  doctest KitchenSink.List
-
-  test "index_on" do
-    expected = %{
-      %{a: 1} => 1,
-      %{a: 2} => 2,
-      %{a: 3} => 3
-    }
-
-    input = [
-      %{a: 1, b: 1},
-      %{a: 2, b: 2},
-      %{a: 3, b: 3}
-    ]
-
-    assert L.index_on(input, [:a], :b) == expected
-    assert L.index_on(input, :a, :b) == expected
-  end
+  doctest KitchenSink.List, import: true
 
   test "pluck" do
 
@@ -32,38 +15,11 @@ defmodule KitchenSink.ListTest do
 
     assert expected == actual
 
-
-    input = [%{b: 1}, %{a: 2}, %{a: 3}]
-    expected = [nil, 2, 3]
-
-    actual = L.pluck(input, :a)
-
-    assert expected == actual
-
-
     input = []
     expected = []
 
     actual = L.pluck(input, :a)
 
     assert expected == actual
-  end
-
-  describe "index_by/2" do
-    test "index by key"do
-      expected = %{
-        a: %{name: :a, other: 1},
-        b: %{name: :b, other: 4}
-      }
-
-      input = [
-        %{name: :a, other: 1},
-        %{name: :b, other: 4}
-      ]
-
-      actual = L.index_by(input, :name)
-
-      assert expected == actual
-    end
   end
 end
