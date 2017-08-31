adebca179746c04a30c1da4ffd2587738470818f
Robert Peacock <robertpeacock22@gmail.com>
2017-08-31 00:42:28 -0400

    Add functions to create random number ranges
    These functions are for use in randomised tests

--- test/kitchen_sink/algorithms_test.exs
+++ test/kitchen_sink/algorithms_test.exs
@@ -29,4 +29,29 @@ defmodule KitchenSink.AlgorithmsTest do
   test "Interval with reversed endpoints" do
     assert {:ok, 3.5} == Algorithms.binary_interval_search(5, 1, &find_target/2, 3.5)
   end
+
+  # FIXME: Maybe make a Random module for these to live in?
+  # Specify a range of `range_size` numbers that are all positive
+  defp random_integer_range(:positive, range_size) when range_size > 1 do
+    lower_bound = :rand.uniform(1000)
+    upper_bound = lower_bound + range_size - 1
+
+    {lower_bound, upper_bound}
+  end
+  # Specify a range of `range_size` numbers that are all negative
+  defp random_integer_range(:negative, range_size) when range_size > 1 do
+    upper_bound = 0 - :rand.uniform(1000)
+    lower_bound = upper_bound - range_size - 1
+
+    {lower_bound, upper_bound}
+  end
+  # Specify a range of `range_size` numbers that range from negative to positive
+  defp random_integer_range(:straddled, range_size) when range_size > 2 do
+    over_random = range_size / 2 |> round()
+
+    upper_bound = :rand.uniform(over_random)
+    lower_bound = upper_bound - range_size + 1
+
+    {lower_bound, upper_bound}
+  end
 end
