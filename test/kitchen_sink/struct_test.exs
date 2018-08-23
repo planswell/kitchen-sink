bf1e714a93b110e152b12860024687d4f95e7bac
Olafur Arason <olafura@olafura.com>
2018-08-23 16:50:58 -0400

    Make clean struct in to_map an option

--- test/kitchen_sink/struct_test.exs
+++ test/kitchen_sink/struct_test.exs
@@ -7,10 +7,32 @@ defmodule KitchenSink.StructTest do
 
   describe "to_map()" do
     test "empty struct converts to map with no fields" do
-      assert Struct.to_map(%TestStruct{}) == %{}
+      assert Struct.to_map(%TestStruct{}) == %{age: nil, name: nil}
+    end
+
+    test "empty struct converts to map with no fields clean struct" do
+      assert Struct.to_map(%TestStruct{}, clean_struct: true) == %{}
     end
 
     test "nested struct converts to map" do
+      input = %TestStructNested{
+        field1: %TestStruct{},
+        field2: %TestStruct{},
+      }
+      expected = %{
+        field1: %{
+          age: nil,
+          name: nil
+        },
+        field2: %{
+          age: nil,
+          name: nil
+        }
+      }
+      assert Struct.to_map(input) == expected
+    end
+
+    test "nested struct converts to map clean struct" do
       input = %TestStructNested{
         field1: %TestStruct{},
         field2: %TestStruct{},
@@ -21,7 +43,7 @@ defmodule KitchenSink.StructTest do
         field2: %{
         }
       }
-      assert Struct.to_map(input) == expected
+      assert Struct.to_map(input, clean_struct: true) == expected
     end
 
     test "struct with list of primitives converts to map" do
@@ -31,10 +53,25 @@ defmodule KitchenSink.StructTest do
       }
       expected = %{
         field1: %{
+          age: nil,
+          name: nil
         },
         field2: ["something", "else"]
       }
       assert Struct.to_map(input) == expected
+    end
+
+    test "struct with list of primitives converts to map clean struct" do
+      input = %TestStructNested{
+        field1: %TestStruct{},
+        field2: ["something", "else"]
+      }
+      expected = %{
+        field1: %{
+        },
+        field2: ["something", "else"]
+      }
+      assert Struct.to_map(input, clean_struct: true) == expected
     end
 
     test "struct with map with nested unwanted key drops key correctly" do
